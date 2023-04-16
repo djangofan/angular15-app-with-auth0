c74560a581b7575274f8e1f76e8068d276d0f1ee
Jon Austen <djangofan@gmail.com>
2023-04-15 18:21:11 -0700

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+
+export const environment = {
+  production: true,
+  auth0: {
+    domain: 'dev-owrcq1v64yv04cev.us.auth0.com',
+    clientId: 'Jd38e8dTb6KFKAZAzgAE3Pb8MwtKiZft',
+    authorizationParams: {
+      redirect_uri: 'http://localhost:4040/callback',
+    },
+  },
+  api: {
+    serverUrl: 'http://localhost:6060',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  auth0: {
+    domain: 'dev-owrcq1v64yv04cev.us.auth0.com',
+    clientId: 'Jd38e8dTb6KFKAZAzgAE3Pb8MwtKiZft',
+    authorizationParams: {
+      redirect_uri: 'http://localhost:4040/callback',
+    },
+  },
+  api: {
+    serverUrl: 'http://localhost:6060',
+  },
+};
